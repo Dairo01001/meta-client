8ab28800f45ad5421095c5136cdd887b4b211b7a
Dairo García Naranjo <50602257+Dairo01001@users.noreply.github.com>
2025-01-13 18:40:20 -0500

    update About

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,45 +1,44 @@
-import { Card, CardProps } from "./components";
+import { Card, CardProps } from './components'
 
 const owners: CardProps[] = [
   {
-    image: "/owners/ortiz.png",
-    title: "Analista de Software",
-    fullName: "Angie Alexandra Ortiz Palacios",
+    image: '/owners/dairo.png',
+    title: 'Desarrollador Full Stack',
+    fullName: 'Dairo García Naranjo'
   },
   {
-    image: "/owners/toquica.png",
-    title: "Full Stack Developer",
-    fullName: "Jose Daniel Toquica Agudelo",
+    image: '/owners/toquica.png',
+    title: 'Full Stack Developer',
+    fullName: 'Jose Daniel Toquica Agudelo'
   },
   {
-    image: "/owners/cenon.png",
-    title: "Diseñador 3D",
-    fullName: "Yerson Arley Cenon Cabrera",
+    image: '/owners/cenon.png',
+    title: 'Diseñador 3D',
+    fullName: 'Yerson Arley Cenon Cabrera'
   },
   {
-    image: "/owners/gallejo.png",
-    title: "Lider de Proyecto",    
-    fullName: "Lubeimar Eduardo Gallejo Ruiz",
+    image: '/owners/gallejo.png',
+    title: 'Lider de Proyecto',
+    fullName: 'Lubeimar Eduardo Gallejo Ruiz'
   },
   {
-    image: "/owners/verastegui.png",
-    title: "Administrador base de datos",
-    fullName: "Vera Estégui Estévez",
+    image: '/owners/verastegui.png',
+    title: 'Administrador base de datos',
+    fullName: 'Fredy Antonio Verástegui González'
   },
   {
-    image: "/owners/millan.png",
-    title: "Arquitecto de Software",
-    fullName: "Edwin Eduardo Millán Rojas",
+    image: '/owners/millan.png',
+    title: 'Arquitecto de Software',
+    fullName: 'Edwin Eduardo Millán Rojas'
   }
 ]
 
-
 export const About = () => {
   return (
-    <div className="flex-grow flex flex-col">
-      <div className="flex-grow p-10 w-full flex flex-col items-center justify-center">
+    <div className="flex flex-grow flex-col">
+      <div className="flex w-full flex-grow flex-col items-center justify-center p-10">
         <h2 className="text-center text-2xl">¿Que es Uniamazonia 3D?</h2>
-        <div className="flex gap-3 items-center">
+        <div className="flex items-center gap-3">
           <img src="/logo.svg" alt="Uniamazonia 3D" className="h-52 w-auto" />
           <p className="max-w-2xl">
             Los estudiantes y profesores podrán explorar el campus virtualmente,
@@ -54,19 +53,29 @@ export const About = () => {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center bg-secondary">
-        <span className="text-3xl text-center m-10 text-green-500">Integrantes</span>
-        <div className="flex flex-wrap gap-3 justify-center">
-          {owners.map((owner) => (
+        <span className="m-10 text-center text-3xl text-green-500">
+          Integrantes
+        </span>
+        <div className="flex flex-wrap justify-center gap-3">
+          {owners.map(owner => (
             <Card key={owner.fullName} {...owner} />
           ))}
         </div>
-        <div className="flex gap-3 items-center justify-center mt-4">
-          <img src="/logo-uniamazonia.png" alt="Logo Universidad de la Amazonia" className="h-24 w-auto" />
-          <img src="/logo-semillero-red.png" alt="Logo Semillero Red" className="h-24 w-auto" />
+        <div className="mt-4 flex items-center justify-center gap-3">
+          <img
+            src="/logo-uniamazonia.png"
+            alt="Logo Universidad de la Amazonia"
+            className="h-24 w-auto"
+          />
+          <img
+            src="/logo-semillero-red.png"
+            alt="Logo Semillero Red"
+            className="h-24 w-auto"
+          />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default About;
+export default About
